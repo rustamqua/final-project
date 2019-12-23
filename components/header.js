328daff6692baa7564783b12742d834002d82dac
Rustam-Deniz Emirali <emirali.rustam.deniz@gmail.com>
2019-12-24 00:21:21 +0600

    fir IOS bug

--- components/header.js
+++ components/header.js
@@ -5,7 +5,10 @@ const Header = () => {
     <div>
       <Head>
         <title>futstore</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"
+        ></meta>
         <link
           href="https://fonts.googleapis.com/css?family=Fira+Sans+Extra+Condensed&display=swap"
           rel="stylesheet"
